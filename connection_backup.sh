1fdded00b93619179da1923208cd85343a31ada0
BerkePoslu <posluberke@gmail.com>
2024-07-05 09:08:25 +0200

    chore: added functions

--- connection_backup.sh
+++ connection_backup.sh
@@ -40,32 +40,40 @@ if ! command -v docker &> /dev/null; then
 fi
 
 
-# Create the backup directory if it doesn't exist
-mkdir -p "${BACKUP_PATH}/backups_compressed"
+perform_backup() {
+    mkdir -p "${BACKUP_PATH}/backups_compressed"
 
-if [ -z "$RUNNING_IN_TEST_ENV" ]; then
-    # Perform the backup using mysqldump within the Docker container and save it to the backup directory
-    sudo docker exec mariadb mariadb-dump -u "${DB_USER}" -p"${DB_PASSWORD}" -h "${DB_ADDRESS}" -P "${DB_PORT}" "${DB_NAME}" > "${BACKUP_PATH}/${BACKUP_NAME}"
-    exit_status=$?
-fi
+    if [ -z "$RUNNING_IN_TEST_ENV" ]; then
+        # Perform the backup using mysqldump within the Docker container and save it to the backup directory
+        sudo docker exec mariadb mariadb-dump -u "${DB_USER}" -p"${DB_PASSWORD}" -h "${DB_ADDRESS}" -P "${DB_PORT}" "${DB_NAME}" > "${BACKUP_PATH}/${BACKUP_NAME}"
+        exit_status=$?
+    fi
 
+    # Check if the backup was successful
+    if [ $exit_status -eq 0 ]; then
+        echo "Database backup was successful."
+        compress_backup
+    else
+        echo "Backup failed."
+        exit $exit_status
+    fi
+}
 
-# Check if the backup was successful
-if [ $? -eq 0 ]; then
-    echo "Database backup was successful."
-    # make a seperate compressed copy of the backup file for sharing with others
+compress_backup() {
     if [ -z "$RUNNING_IN_TEST_ENV" ]; then
         gzip -c "${BACKUP_PATH}/${BACKUP_NAME}" > "${BACKUP_PATH}/${BACKUP_NAME}.gz"
         # Move the compressed backup file to the backups_compressed folder
         mv "${BACKUP_PATH}/${BACKUP_NAME}.gz" "${BACKUP_PATH}/backups_compressed/${BACKUP_NAME}.gz"
         echo "Backup compressed."
     fi
-else
-    echo "Backup failed."
-    exit $exit_status
-fi
+}
 
-# Cleanup old backups (older than BACKUP_DAYS)
-find "${BACKUP_PATH}" ! -type d -name "*.sql" -mtime +${BACKUP_DAYS} -delete
-find "${BACKUP_PATH}/backups_compressed" ! -type d -name "*.sql.gz" -mtime +${BACKUP_DAYS} -delete
-echo "Old backups cleaned up."
+cleanup_old_backups() {
+    # Cleanup old backups (older than BACKUP_DAYS)
+    find "${BACKUP_PATH}" ! -type d -name "*.sql" -mtime +${BACKUP_DAYS} -delete
+    find "${BACKUP_PATH}/backups_compressed" ! -type d -name "*.sql.gz" -mtime +${BACKUP_DAYS} -delete
+    echo "Old backups cleaned up."
+}
+
+perform_backup
+cleanup_old_backups
